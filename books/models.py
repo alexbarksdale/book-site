065b5a9c18325f12cfb46355ca8b277abbe7e150
alexbarksdale <ale.barksdale@gmail.com>
2020-04-13 23:28:51 -0500

    Gradescope fix?

--- books/models.py
+++ books/models.py
@@ -12,7 +12,7 @@ class Author(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
